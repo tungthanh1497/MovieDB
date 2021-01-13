155a88f2a1636710edb150dca8239abdcbeea1ff
tungtt <tungthanh1497@gmail.com>
2021-01-13 11:02:49 +0700

    data binding in MovieAdapter.kt

--- app/src/main/java/com/tungtt/moviedb/ui/main/adapter/MovieAdapter.kt
+++ app/src/main/java/com/tungtt/moviedb/ui/main/adapter/MovieAdapter.kt
@@ -1,15 +1,14 @@
 package com.tungtt.moviedb.ui.main.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.squareup.picasso.Picasso
 import com.tungtt.moviedb.R
+import com.tungtt.moviedb.databinding.ItemMovieBinding
 import com.tungtt.moviedb.model.MovieModel
-import kotlinx.android.synthetic.main.item_movie.view.*
 
 class MovieAdapter(_callback: OnMovieAdapterListener) :
     ListAdapter<MovieModel, MovieAdapter.MovieAdapterViewholder>(MovieAdapterDiffCallback()) {
@@ -17,24 +16,26 @@ class MovieAdapter(_callback: OnMovieAdapterListener) :
     private val callback = _callback
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieAdapterViewholder {
-        return MovieAdapterViewholder(
-            LayoutInflater.from(parent.context)
-                .inflate(R.layout.item_movie, parent, false)
+        val binding: ItemMovieBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_movie,
+            parent,
+            false
         )
+        return MovieAdapterViewholder(binding)
     }
 
     override fun onBindViewHolder(holder: MovieAdapterViewholder, position: Int) {
         holder.bind(getItem(position), callback)
     }
 
-    class MovieAdapterViewholder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class MovieAdapterViewholder(_binding: ItemMovieBinding) :
+        RecyclerView.ViewHolder(_binding.root) {
+
+        val binding = _binding
         fun bind(item: MovieModel, callback: OnMovieAdapterListener) = with(itemView) {
-            nameTextView.text = item.title
-            ratingTextView.text = "Rating: ${item.voteAverage}"
-            popularityTextView.text = "${item.popularity} views"
-            Picasso.get()
-                .load("https://image.tmdb.org/t/p/w600_and_h900_bestv2/${item.posterPath}")
-                .into(bannerImageView)
+
+            binding.movieModel = item
 
             setOnClickListener {
                 callback.onMovieClicked(item.id)
